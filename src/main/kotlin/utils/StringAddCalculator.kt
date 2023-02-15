7c2a293b7c21023870331e00c2c790334ae375f6
suhongkim98 <kshz9815@naver.com>
2023-02-15 10:54:44 +0900

    refactoring

--- src/main/kotlin/utils/StringAddCalculator.kt
+++ src/main/kotlin/utils/StringAddCalculator.kt
@@ -10,7 +10,7 @@ class StringAddCalculator {
         private val regex = Regex(CUSTOM_SEPARATOR_PATTERN)
 
         fun splitAndSum(input: String?): Int {
-            if(input.isNullOrEmpty()) {
+            if (input.isNullOrEmpty()) {
                 return 0
             }
 
@@ -24,32 +24,25 @@ class StringAddCalculator {
             }
         }
 
-        private fun getTarget(input: String): String {
-            return if(isCustomSeparatorInput(input)) {
-                regex.matchEntire(input)!!.groups[2]!!.value
-            } else {
-                input
-            }
+        private fun getTarget(input: String): String = if (isCustomSeparatorInput(input)) {
+            regex.matchEntire(input)!!.groups[2]!!.value
+        } else {
+            input
         }
 
-        private fun isCustomSeparatorInput(input: String): Boolean {
-            return regex.matches(input)
-        }
+        private fun isCustomSeparatorInput(input: String): Boolean = regex.matches(input)
 
-        private fun getSeparators(input: String): Array<String> {
-            // 커스텀 구분자 여부 확인
-            return if(isCustomSeparatorInput(input)) {
-                arrayOf(regex.matchEntire(input)!!.groups[1]!!.value)
-            } else {
-                DEFAULT_DELIMITERS
-            }
+        private fun getSeparators(input: String): Array<String> = if (isCustomSeparatorInput(input)) {
+            arrayOf(regex.matchEntire(input)!!.groups[1]!!.value)
+        } else {
+            DEFAULT_DELIMITERS
         }
 
         private fun getSum(target: String, vararg separator: String): Int {
             var sum = 0
-            for(item in target.split(delimiters = separator)) {
+            for (item in target.split(delimiters = separator)) {
                 val value = item.toInt()
-                if(value < 0) throw RuntimeException()
+                if (value < 0) throw RuntimeException()
                 sum += value
             }
             return sum
