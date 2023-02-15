12da283415768a86655c65d22417250c691a181e
suhongkim98 <kshz9815@naver.com>
2023-02-15 10:40:43 +0900

    2차 과제 제출

---
+++ src/main/kotlin/utils/StringAddCalculator.kt
@@ -0,0 +1,58 @@
+package utils
+
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+class StringAddCalculator {
+    companion object {
+        private const val CUSTOM_SEPARATOR_PATTERN = "//(.)\n(.*)"
+        private val DEFAULT_DELIMITERS = arrayOf(",", ":")
+        private val regex = Regex(CUSTOM_SEPARATOR_PATTERN)
+
+        fun splitAndSum(input: String?): Int {
+            if(input.isNullOrEmpty()) {
+                return 0
+            }
+
+            val target = getTarget(input)
+            val separator = getSeparators(input)
+
+            try {
+                return getSum(target, separator = separator)
+            } catch (e: NumberFormatException) {
+                throw RuntimeException()
+            }
+        }
+
+        private fun getTarget(input: String): String {
+            return if(isCustomSeparatorInput(input)) {
+                regex.matchEntire(input)!!.groups[2]!!.value
+            } else {
+                input
+            }
+        }
+
+        private fun isCustomSeparatorInput(input: String): Boolean {
+            return regex.matches(input)
+        }
+
+        private fun getSeparators(input: String): Array<String> {
+            // 커스텀 구분자 여부 확인
+            return if(isCustomSeparatorInput(input)) {
+                arrayOf(regex.matchEntire(input)!!.groups[1]!!.value)
+            } else {
+                DEFAULT_DELIMITERS
+            }
+        }
+
+        private fun getSum(target: String, vararg separator: String): Int {
+            var sum = 0
+            for(item in target.split(delimiters = separator)) {
+                val value = item.toInt()
+                if(value < 0) throw RuntimeException()
+                sum += value
+            }
+            return sum
+        }
+    }
+}
---
+++ src/test/kotlin/utils/StringCalculatorTest.kt
@@ -0,0 +1,81 @@
+package utils
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import java.lang.RuntimeException
+
+internal class StringCalculatorTest : DescribeSpec({
+
+    describe("splitAndSum 메서드는") {
+
+        context("null 이 주어지면") {
+            val givenInput = null
+            it("0을 반환한다.") {
+                // test here
+                StringAddCalculator.splitAndSum(givenInput) shouldBe 0
+            }
+        }
+
+        context("빈 문자열이 주어지면") {
+            val givenInput = ""
+            it("0을 반환한다.") {
+                // test here
+                StringAddCalculator.splitAndSum(givenInput) shouldBe 0
+            }
+        }
+
+        context("숫자 하나가 주어지면") {
+            val givenInput = "1"
+            it("그 숫자를 반환한다.") {
+                // test here
+                StringAddCalculator.splitAndSum(givenInput) shouldBe givenInput.toInt()
+            }
+        }
+
+        context("쉼표 구분자(,)로 여러 숫자를 넣으면") {
+            val givenInput = "1,2,3"
+            it("그 숫자들의 합을 반환한다.") {
+                // test here
+                StringAddCalculator.splitAndSum(givenInput) shouldBe 6
+            }
+        }
+
+        context("쉼표 구분자(,)와 콜론 구분자(:)를 둘 다 써서 여러 숫자를 넣으면") {
+            val givenInput = "1,2:3"
+            it("그 숫자들의 합을 반환한다.") {
+                // test here
+                StringAddCalculator.splitAndSum(givenInput) shouldBe 6
+            }
+        }
+
+        context("문자열 앞부분의 '//'와 \\n 사이에 커스텀 구분자를 지정하여") {
+            val givenInput = "//;\n1;2;3"
+            it("그 숫자들의 합을 반환한다.") {
+                // test here
+                StringAddCalculator.splitAndSum(givenInput) shouldBe 6
+            }
+        }
+
+        context("음수가 들어가면") {
+            val givenInput = "-1"
+            it("RuntimeException 을 발행한다.") {
+                // test here
+                shouldThrow<RuntimeException> {
+                    StringAddCalculator.splitAndSum(givenInput)
+                }
+            }
+        }
+
+        context("숫자 이외에 문자가 들어가면") {
+            val givenInput = "hello"
+            it("RuntimeException 을 발행한다.") {
+                // test here
+                shouldThrow<RuntimeException> {
+                    StringAddCalculator.splitAndSum(givenInput)
+                }
+            }
+        }
+    }
+
+})
