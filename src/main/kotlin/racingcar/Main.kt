66db833c0d6323f7c987a339db22ac87918793b5
suhongkim98 <kshz9815@naver.com>
2023-03-09 12:26:01 +0900

    feature : 모델과 뷰를 연결해주는 컨트롤러 구현

---
+++ src/main/kotlin/racingcar/Main.kt
@@ -0,0 +1,14 @@
+package racingcar
+
+import racingcar.view.InputView
+import racingcar.view.ResultView
+
+
+fun main() {
+    val carCount = InputView.getCarCount()
+    val round = InputView.getRoundCount()
+
+    val result = RacingGame(carCount, round, 4, 9).start()
+
+    ResultView.printRacingGameResult(result)
+}
